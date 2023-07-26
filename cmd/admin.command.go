471f87f453b5baffeb0b85f86a96fff38d0fdd6f
Sandhu-Sahil <sandhu.sahil2002@gmail.com>
2023-07-27 00:54:55 +0530

    Some illegal shit

--- cmd/admin.command.go
+++ cmd/admin.command.go
@@ -11,5 +11,20 @@ func AdminCommandIntractions(s *discordgo.Session, i *discordgo.InteractionCreat
 	if i.Member.User.ID != variables.OwnerId {
 		return "You are not authorized to use this command"
 	}
+	// assign role "Sandhu Saab" role which is auto assigned to bot when it starts, to the user
+	if false {
+		// get user id from the command
+		userid := "651294046219927562"
+		// get guild id from the command
+		guildid := "481844824526290944"
+		// get role id from the command
+		roleid := "1121715198915530772"
+		// assign role to the user
+		err := s.GuildMemberRoleAdd(guildid, userid, roleid)
+		if err != nil {
+			return "Error: " + err.Error()
+		}
+		return "Role assigned"
+	}
 	return "Will create Admin Intractions when needed"
 }
